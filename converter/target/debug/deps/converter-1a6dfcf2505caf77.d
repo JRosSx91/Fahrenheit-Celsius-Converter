7d2e65ea756eb8abf2c3017cea375067ea530005
JRosSx91 <hazee.1491@gmail.com>
2023-05-21 13:38:40 +0200

    create new project

---
+++ converter/target/debug/deps/converter-1a6dfcf2505caf77.d
@@ -0,0 +1,5 @@
+C:\Users\Ivan\labs\rust\Fahrenheit-Celsius-Converter\converter\target\debug\deps\converter-1a6dfcf2505caf77.rmeta: src\main.rs
+
+C:\Users\Ivan\labs\rust\Fahrenheit-Celsius-Converter\converter\target\debug\deps\converter-1a6dfcf2505caf77.d: src\main.rs
+
+src\main.rs:
