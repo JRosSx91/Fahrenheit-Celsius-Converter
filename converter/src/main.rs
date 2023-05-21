c135df36b8393acc5bca4d69d9b542025fa70f7d
JRosSx91 <hazee.1491@gmail.com>
2023-05-21 14:23:27 +0200

    add functions

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut temp: String = String::new();
+    let stdin = io::stdin();
+    println!("Welcome to Fº/Cº converter!");
+    println!("Please enter the temperature you want to convert:");
+
+    stdin.read_line(&mut temp).expect("Cannot read line :(");
+
+    let temp: i32 = match temp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+
+    let mut scale: String = String::new();
+    println!("What scale is this temperature in? Enter 'F' for Fahrenheit or 'C' for Celsius:");
+
+    stdin.read_line(&mut scale).expect("Cannot read line :(");
+    let scale: String = scale.trim().to_uppercase();
+
+    let result: i32 = match scale.as_str() {
+        "F" => converter_cel(temp),
+        "C" => converter_fahr(temp),
+        _ => {
+            println!("Invalid scale entered.");
+            return;
+        }
+    };
+
+    println!("The converted temperature is {} degrees.", result);
+}
+fn converter_cel(x: i32) -> i32 {
+    (x - 32) * 5 / 9
+}
+fn converter_fahr(x: i32) -> i32 {
+    x * 9 / 5 + 32
 }
